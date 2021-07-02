16988d6bfd8a11538ee69149f70cae8cc9f09865
molese <molese@protonmail.com>
2021-07-02 21:01:17 +0600

    comment fish ssh agent & add fish ssh eval notice

--- fish/config.fish
+++ fish/config.fish
@@ -223,42 +223,44 @@ export DOTNET_CLI_TELEMETRY_OPTOUT="true"
 alias proton="STEAM_COMPAT_DATA_PATH='/home/user/.proton' PROTON_NO_ESYNC=1 PROTON_DUMP_DEBUG_COMMANDS='1' strat -r arch /home/user/.steam/steam/steamapps/common/Proton\ 5.13/proton run"
 
 # SSH
+#- eval (keychain --eval --agents ssh -Q --quiet ~/.ssh/id_ed25519 --nogui)
 # content has to be in .config/fish/config.fish
 # if it does not exist, create the file
-setenv SSH_ENV $HOME/.ssh/environment
+#setenv SSH_ENV $HOME/.ssh/environment
 
-function start_agent                                                                                                                                                                    
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV 
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
 
-function test_identities                                                                                                                                                                
-    ssh-add -l | grep "The agent has no identities" > /dev/null
-    if [ $status -eq 0 ]
-        ssh-add
-        if [ $status -eq 2 ]
-            start_agent
-        end
-    end
-end
+#function start_agent                                                                                                                             
+#    echo "Initializing new SSH agent ..."
+#    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+#    echo "succeeded"
+#    chmod 600 $SSH_ENV
+#    . $SSH_ENV > /dev/null
+#    ssh-add
+#end
 
-if [ -n "$SSH_AGENT_PID" ] 
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    end  
-else
-    if [ -f $SSH_ENV ]
-        . $SSH_ENV > /dev/null
-    end  
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    else 
-        start_agent
-    end  
-end
+#function test_identities                                                                                                                         
+#    ssh-add -l | grep "The agent has no identities" > /dev/null
+#    if [ $status -eq 0 ]
+#        ssh-add
+#        if [ $status -eq 2 ]
+#            start_agent
+#        end
+#    end
+#end
+
+#if [ -n "$SSH_AGENT_PID" ]
+#    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+#    if [ $status -eq 0 ]
+#        test_identities
+#    end
+#else
+#    if [ -f $SSH_ENV ]
+#        . $SSH_ENV > /dev/null
+#    end
+#    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+#    if [ $status -eq 0 ]
+#        test_identities
+#    else
+#        start_agent
+#    end
+#end
